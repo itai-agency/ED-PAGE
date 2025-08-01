70851236883ee773c5a547cb243b9a797313b05a
Gelvins D. Franco <gelvins15@gmail.com>
2025-08-01 21:18:06 +0000

    disminuye un poco el degradado en la web

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&family=Roboto:wght@400;600;700&display=swap" rel="stylesheet" />
       </head>
       <body className="antialiased">
-        <div className="flex flex-col min-h-screen bg-gradient-to-b from-background to-primary/10">
+        <div className="flex flex-col min-h-screen bg-gradient-to-b from-background to-primary/5">
             <Header />
             <main className="flex-grow">{children}</main>
             <Footer />
